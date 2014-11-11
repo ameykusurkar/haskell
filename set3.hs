30bcbdb0d3c06235fa1691dbc5fc9deeb8274294
Amey Kusurkar <ameykusurkar@dyn1207-118.wlan.ic.ac.uk>
2014-11-11 01:24:15 +0000

    set3 completed until qn17

--- set3.hs
+++ set3.hs
@@ -1,6 +1,7 @@
 module Set3 where
 
 import Data.List
+import Data.Char
 
 precedes :: String -> String -> Bool
 -- Returns True if first string is lexicographically less than second
@@ -56,4 +57,75 @@ backwards xs = backwards' xs []
     backwards' []     acc = acc
     backwards' (x:xs) acc = backwards' xs (x:acc)
 
--- transpose :: String -> String -> String
+
+transpose :: String -> String -> String -> String
+-- Transposes a string based on a transposition defined by 2 strings
+-- Pre: All 3 strings are of the same length
+-- Set3.transpose defined because of name clashing with Prelude
+transpose str from (t:ts)
+  = ( str !! (pos t from) ) : (Set3.transpose str from ts)
+transpose str from []
+  = []
+
+
+substring :: String -> String -> Bool
+-- Returns True is the first string is a substring of the second
+substring test (x:xs)
+ = and ( zipWith (==) test (x:xs) ) || substring test xs
+substring test []
+ = False
+
+
+nextWord :: String -> ( String, String )
+-- Returns a tuple of the next word and the rest of the string 
+nextWord [] = ([], [])
+nextWord (first:rest)
+  | isSpace first = ([], rest)
+  | otherwise     = (first : word, string)
+    where
+      (word, string) = nextWord rest
+
+
+splitUp :: String -> [String]
+-- Returns list of words in the string
+splitUp []  = []
+splitUp (c:cs) 
+  | isSpace c = splitUp cs
+  | otherwise = first : splitUp rest
+  where
+    (first, rest) = nextWord (c:cs)
+
+
+merge2 :: [Int] -> [Int] -> [Int]
+-- Merges 2 ordered lists
+merge2 xs [] = xs
+merge2 [] ys = ys
+merge2 (x:xs) (y:ys)
+  | x < y    = x : ( merge2 xs (y:ys) )
+  |otherwise = y : ( merge2 (x:xs) ys )
+
+
+timesTable :: Int -> Int -> [String]
+-- Returns a list of timestables upto a*b
+timesTable a b = [disp x y | x <- [1..a], y <- [1..b] ]
+  where
+    disp x y = show x ++ " times " ++ show y ++ " is " ++ show (x*y)
+
+
+qsort :: [Int] -> [Int]
+-- Sorts a list of ints
+qsort []     = [] 
+qsort (i:is) = (qsort lesser) ++ [i] ++ (qsort greater)
+  where 
+    lesser  = [ x | x <- is, x <= i ]
+    greater = [ y | y <- is, y > i  ]  
+
+
+perms :: Eq a => [a] -> [[a]]
+-- Returns a list of all the permutations of a list
+perms list = [ list \\ [l] | l <- list ]
+
+
+
+
+
